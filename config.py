5ca1a2098de57b66b7d2555083c087581a2478f9
yutakinn <163711581+yutakinn@users.noreply.github.com>
2024-07-22 16:13:02 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '3d6f45a5fc12445dbac2f59c3b6c7cb1d3c11316384af4638bbc8e5f0babcc2f'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASE_DIR, 'dog_spots.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError("No SECRET_KEY set for Flask application")
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if not SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'dog_spots.db')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
